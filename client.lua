55acc6d1354238d6fd580454adccc7e30cc0382b
BlazingTwist <blazingtwist1@gmail.com>
2023-07-01 13:10:00 +0200

    - add event for saving state before afk-kick - add event for marking the player as 'not afk'

--- client.lua
+++ client.lua
@@ -3,10 +3,13 @@
 -- AFK Kick Time Limit (in seconds)
 secondsUntilKick = 1800
 
--- Warn players if 3/4 of the Time Limit ran up
-kickWarning = false
-
 -- CODE --
+
+secondsLeft = secondsUntilKick
+
+AddEventHandler("empire_afk:PlayerNotAfk", function()
+	secondsLeft = secondsUntilKick
+end)
 
 Citizen.CreateThread(function()
 	while true do
@@ -17,17 +20,15 @@ Citizen.CreateThread(function()
 			currentPos = GetEntityCoords(playerPed, true)
 
 			if currentPos == prevPos then
-				if time > 0 then
-					if kickWarning and time == math.ceil(secondsUntilKick / 4) then
-						--TriggerEvent("chatMessage", "Warnung!", {255, 0, 0}, "^1Du wirst in" .. time .. " Sekunden wegen Inaktivität gekickt!")
-					end
-
-					time = time - 1
+				if secondsLeft > 0 then
+					secondsLeft = secondsLeft - 1
 				else
+					TriggerEvent('PlayerKick_SaveState')
+					Wait(5000) -- wait 5 seconds so that active scripts can save their state before the player is kicked
 					TriggerServerEvent("kickForBeingAnAFKDouchebag")
 				end
 			else
-				time = secondsUntilKick
+				secondsLeft = secondsUntilKick
 			end
 
 			prevPos = currentPos
